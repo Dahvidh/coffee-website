6b4d49d0e1878255d0aaa97dcb87166c5a217cf9
David Nnamdi <95830108+Dahvidh@users.noreply.github.com>
2023-11-18 17:36:56 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Landingpage from './components/Landingpage';
+import ScrollReveal from 'scrollreveal';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Landingpage/>
+    </div>
+  );
+}
+
+export default App;
